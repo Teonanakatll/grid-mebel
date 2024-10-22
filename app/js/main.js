3660f644695ea66d2be53675cc4655f5c7dda588
Teonanakatll <105820090+Teonanakatll@users.noreply.github.com>
2024-10-22 12:11:17 +0300

    commit 1

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,35 @@
-// import Swiper from "swiper"
+import Swiper from "swiper"
 
 // import {animate} from "./gsap-test"
 import { setBackgrounds } from "./set-background"
 
-// import { Parallax, Mousewheel, Controller, Pagination, Scrollbar, Navigation, Thumbs, Autoplay } from 'swiper/modules'
-// import { jqueryTest } from "./jquery";
+import { Parallax, Mousewheel, Controller, Pagination, Scrollbar, Navigation, Thumbs, Autoplay, EffectFade } from 'swiper/modules'
+import { myJquery } from "./jquery";
 
 document.addEventListener('DOMContentLoaded', () => {
 
+	const sliderTop = new Swiper('.top__slider', {
+		modules: [Pagination, Parallax, Autoplay, EffectFade],
+		pagination: {
+			el: '.top__slider-pagination',
+			clickable: true,
+		},
+		loop: true,
+		// autoplay: {
+		// 	delay: 3000,
+		// 	pauseOnMouseEnter: true,
+		// },
+		effect: 'fade',
+		fadeEffect: {
+			crossFade: true,
+		},
+		speed: 2000,
+		parallax: true,
+	})
 
 	// animate();
 
-	// jqueryTest();
+	myJquery();
 
 	// скрипт автоматически находит классы элементов начинающихся с  bg-- и подставляет нужный формат изображения
 	// также необходимо прописать фоллбак на случай если у пользователя отключен js: style="background-image: url('images/header-bg.jpg');"
